9effc02e88df6875e4734cbbe95f264d2a8b905a
Jędrzej Szczepaniak <je.szczepaniak@gmail.com>
2016-05-16 17:08:38 +0200

    Some fixes

--- src/main/java/pl/edu/pwr/pp/AsciiArtApplication.java
+++ src/main/java/pl/edu/pwr/pp/AsciiArtApplication.java
@@ -6,6 +6,9 @@ public class AsciiArtApplication {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Program start...");
+		System.out.println();
+		
 		String[] images = new String[]{"Marilyn_Monroe", "Mona_Lisa", "Sierpinski_Triangle"};
 		String pgmExtension = ".pgm";
 		String txtExtension = ".txt";
@@ -16,14 +19,21 @@ public class AsciiArtApplication {
 		for (String imageName : images) {
 			try {
 				// przeczytaj plik pgm
+				System.out.println("Odczyt pliku: " + imageName);
 				int[][] intensities = imageFileReader.readPgmFile(imageName + pgmExtension);
 				// przekształć odcienie szarości do znaków ASCII
+				System.out.println("Konwersja pliku: " + imageName);
 				char[][] ascii = ImageConverter.intensitiesToAscii(intensities);
 				// zapisz ASCII art do pliku tekstowego
+				System.out.println("Zapis pliku: " + imageName);
 				imageFileWriter.saveToTxtFile(ascii, imageName + txtExtension);
+				
+				System.out.println();
 			} catch (URISyntaxException e) {
 				e.printStackTrace();
 			} 
 		}
+		
+		System.out.println("DONE");
 	}
 }
